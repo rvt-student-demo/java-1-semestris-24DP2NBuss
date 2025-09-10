ac636ddc92604ddf860eaf39ba34a9b288072455
24DP2NBuss <A240199NB@rvt.lv>
2025-09-10 13:16:08 +0300

    various variables

--- src/main/java/rvt/various_variables.java
+++ src/main/java/rvt/various_variables.java
@@ -6,20 +6,22 @@ public class various_variables {
         int Chickenamount = 9000;
         double bacon = 0.1;
         String tractorname = "Zetor";
+
         System.out.println("Chicken: ");
-        System.out.println(Chickenamount);
+            System.out.println(Chickenamount);
         System.out.println("Bacon(kg): ");
-        System.out.println(bacon);
-        System.out.println("Tractor");
-        System.out.println(tractorname); 
-        System.out.println(" ");
+            System.out.println(bacon);
+        System.out.println("Tractor: ");
+            System.out.println(tractorname);
+
+            System.out.println(" ");
+
         System.out.println("And finally, a summary:");
-        System.out.println(Chickenamount);
-        System.out.println(bacon);
-        System.out.println(tractorname);
+            System.out.println(Chickenamount);
+            System.out.println(bacon);
+            System.out.println(tractorname);
 
 
     }
     
 }
-
