a151e66807e4056e15b2a99482514fae042ead20
Nikalas Buss <A240199NB@rvt.lv>
2025-11-24 17:59:01 +0630

    Java mooc 2-4

---
+++ src/main/java/rvt/DivisibleByThree.java
@@ -0,0 +1,19 @@
+package rvt;
+
+public class DivisibleByThree {
+    public static void main(String[] args) {
+        divisibleByThreeInRange(0, 18);
+    }
+
+
+        public static void divisibleByThreeInRange(int beginning, int end) {
+
+        
+            for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
